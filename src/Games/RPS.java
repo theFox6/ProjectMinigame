b305717b26122d87e22546f01ab7299d4a3ff2fa
Nils488 <nilsbienert@gmail.com>
2020-01-24 12:02:38 +0100

    RPS
    Schere, Stein, Papier

--- src/Games/RPS.java
+++ src/Games/RPS.java
@@ -2,17 +2,58 @@ package Games;
 
 import GameEngine.PrintingGame;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class RPS extends PrintingGame {
+
+    Scanner scan = new Scanner(System.in);
+
     public RPS(PrintStream out) {
         super("Schere Stein Papier", out);
     }
-    
+
     @Override
     public void run() {
-        p("In Bearbeitung");
         // Benutzereingabe R/P/S
+        p("Für Schere: 1");
+        p("Für Stein: 2");
+        p("Für Papier: 3");
+        p("Wählen Sie eine der Folgenenden Optionen");
+        int opt = scan.nextInt();
+        
         // zufällige Auswahl des Com Spielers
+        int zufall = (int) Math.round(Math.random() * 3) + 1;
         // Auswertung des Ergebnisses
+        if (zufall == 1) {
+            p("Der Com wählt Schere");
+            if (opt == 1) {
+                p("Es ist unentschieden");
+            } else if (opt == 2) {
+                p("Sie Haben gewonnen");
+            } else if (opt == 3) {
+                p("Sie haben verloren");
+            }
+        } else if (zufall == 2) {
+            p("Der Com wählt Stein");
+            if (opt == 1) {
+                p("Sie haben Verloren");
+            } else if (opt == 2) {
+                p("Es ist unentschieden");
+            } else if (opt == 3) {
+                p("Sie haben Gewonnen");
+            }
+        } else if (zufall == 3) {
+            p("Der Com wählt Papier");
+            if (opt == 1) {
+                p("Sie haben Gewonnen");
+            } else if (opt == 2) {
+                p("Sie haben Gewonnen");
+            } else if (opt == 3) {
+                p("Es ist unentschieden");
+            }
+        }
+           else {
+        p("Sie habe eine Falsche Option verwendet");
+        }
     }
 }
