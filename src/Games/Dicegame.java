525d8fea6533ebe23ee4f958626c1691416193bf
Berd75 <stueber.manuel@gmail.com>
2020-02-28 11:58:57 +0100

    Added Dicegame
    Dicegame base

---
+++ src/Games/Dicegame.java
@@ -0,0 +1,18 @@
+package Games;
+
+import GameEngine.PrintingGame;
+
+public class Dicegame extends PrintingGame {
+    public Dicegame() {
+        super("Würfeln");
+    }
+    
+    @Override
+    public void run() {
+        
+        p("In Bearbeitung");
+        p("Willkommen beim Würfeln!");
+        
+        
+    }
+}
