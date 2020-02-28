d84177ad674e4571199e258701f3518b585729af
Berd75 <stueber.manuel@gmail.com>
2020-02-28 11:57:27 +0100

    Add field for ships
    Ship

--- src/Games/Battleships.java
+++ src/Games/Battleships.java
@@ -2,14 +2,22 @@ package Games;
 
 import GameEngine.CharField;
 import GameEngine.PrintingGame;
+import GameEngine.Short2DPoint;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Battleships extends PrintingGame {
-    public Battleships(PrintStream out, Scanner in) {
-        super("Schiffe versenken", out, in);
+    public Battleships() {
+        super("Schiffe versenken");
     }
     private static final char[]letters = {'A','B','C','D','E','F','G','H','I'};
+    
+    public void displayField(CharField f) {
+        Short2DPoint origin = new Short2DPoint(0,0);
+        Short2DPoint size = new Short2DPoint(11,11);
+        Short2DPoint scale = new Short2DPoint(2,1);
+        p(f.toString(origin, size, scale));
+    }
     
     @Override
     public void run() {
@@ -20,7 +28,7 @@ public class Battleships extends PrintingGame {
         CharField f = new CharField();
         
         for (int num = 1; num < 10; num++) {
-            f.set(num*2,0,Integer.toString(num).charAt(0));
+            f.set(num,0,Integer.toString(num).charAt(0));
         }
         
         for (int lett = 1; lett < 10; lett++) {
@@ -29,11 +37,14 @@ public class Battleships extends PrintingGame {
 
         for (short yp = 1; yp < 10; yp++) {
             for (short xp = 1; xp < 10; xp++) {
-                f.set(xp*2,yp,'~');
+                f.set(xp,yp,'~');
             }
         }
         
-        p(f.toString(0, 0, 22, 11));
+        displayField(f);
+        CharField s = new CharField();
+        
+        f.toString(0, 0, 22, 11);
         
     } 
 }
