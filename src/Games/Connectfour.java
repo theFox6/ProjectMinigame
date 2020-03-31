f1601b24dbaa9b19d8299e2eedd158c951a18564
Nils488 <60072911+Nils488@users.noreply.github.com>
2020-03-31 17:27:21 +0200

    dokumentation

--- src/Games/Connectfour.java
+++ src/Games/Connectfour.java
@@ -11,38 +11,57 @@ public class Connectfour extends PrintingGame {
 
     @Override
     public void run() {
+
+        /**
+         * deklaration initalization
+         */
         CharField f = new CharField();
         CharField g = new CharField();
-        char colour = ' ', first = 't',n2;
+        char colour = ' ', first = 't', n2;
         String playername = "";
-        Boolean win = false, player = true, abr = false;
-        int width = 0, n = 0, height = 0, y, x, w, fieldsx = 0, fieldsy = 0;
-        // set size
+        Boolean win = false, player = true, stop = false;
+        int width = 0, n = 0, height = 0, y, x, w, fieldsx = 0, fieldsy = 0, RADIX = 10;
+
+        /**
+         * set size loop until size is 4x4 or higher
+         */
         do {
-            p("Wieviele Fields wollen Sie in X-Richtung?");
+            p("Wieviele Felder wollen Sie in X-Richtung?");
+            p("(Die Zahlen werden nur bis 9 gelistet)");
             fieldsx = input.nextInt();
-            p("Wieviele Fields wollen Sie in Y-Richtung?");
+            p("Wieviele Felder wollen Sie in Y-Richtung?");
             fieldsy = input.nextInt();
             if (fieldsx < 4 || fieldsy < 4) {
                 p("Bitte wÃ¤hlen Sie mindestens 4x4!");
             }
         } while (fieldsx < 4 || fieldsy < 4);
 
+        /**
+         * change fieldsx and fieldsy so they fit in the Charfield
+         */
         fieldsx = (fieldsx - 1) * 2;
         fieldsy = (fieldsy - 1) * 2;
         p("");
+
+        /**
+         * set the Charfield set numbers
+         */
         n = 1;
         y = 0;
         for (x = 0; x <= fieldsx; x = x + 2) {
-            final int RADIX = 10;
             n2 = Character.forDigit(n, RADIX);
             g.set(x, y, n2);
             n++;
         }
-        //print field
-          text.print(g.toString(0, 0, fieldsx + 1,1));
-          
-        //Charfield
+
+        /**
+         * printing the Charfield
+         */
+        text.print(g.toString(0, 0, fieldsx + 1, 1));
+
+        /**
+         * set the Charfield set |,+,-
+         */
         // for |
         for (y = 0; y <= fieldsy; y = y + 2) {
             for (x = 1; x <= fieldsx; x = x + 2) {
@@ -61,13 +80,25 @@ public class Connectfour extends PrintingGame {
                 f.set(x, y, '+');
             }
         }
-        //print field
-         text.print(f.toString(0, 0, fieldsx + 1, fieldsy + 1));
-         p("");
-        // the Game
-        while (!win && !abr) {
-            // set the line/row
+
+        /**
+         * printing the other Charfield
+         */
+        text.print(f.toString(0, 0, fieldsx + 1, fieldsy + 1));
+        p("");
+
+        /**
+         * the real game loop as long as no win and chance to win (stop)
+         */
+        while (!win && !stop) {
+
+            /**
+             * loop until ture argument
+             */
             do {
+                /**
+                 * output for the user set the right arguments
+                 */
                 if (player) {
                     p("Spieler 1 ist an der Reihe.");
                     p("Sie haben O.");
@@ -79,28 +110,37 @@ public class Connectfour extends PrintingGame {
                     colour = 'X';
                     playername = "Spieler 2";
                 }
+
+                /**
+                 * set the row to set set width to fit in the Charfiled
+                 */
                 p("In welche Reihe wollen Sie setzen?");
                 width = input.nextInt();
                 width = (width - 1) * 2;
-
                 if (width < 0 || width > fieldsx || f.get(width, 0) != ' ') {
                     p("Bitte wÃ¤hlen Sie eine gÃ¼ltige Reihe.");
                 }
             } while (width < 0 || width > fieldsx || f.get(width, 0) != ' ');
-            // determine the height of each chip
+
+            /**
+             * determine the height of each chip print CHarfield agein
+             */
             for (height = fieldsy; height >= 0; height = height - 2) {
                 if (f.get(width, height) == ' ') {
                     // set the chip
                     f.set(width, height, colour);
-                    // create a new field
-                    text.print(g.toString(0, 0, fieldsx + 1,1));
+                    // print the Charfield
+                    text.print(g.toString(0, 0, fieldsx + 1, 1));
                     text.print(f.toString(0, 0, fieldsx + 1, fieldsy + 1));
                     p("");
                     break;
                 }
             }
-//check win
-            // x=width
+            /**
+             * check if there are 4 in a row controll win
+             */
+
+            // x = width
             if (height <= fieldsy - (3 * 2)) {
                 n = 0;
                 for (y = height; y <= height + (3 * 2); y = y + 2) {
@@ -115,7 +155,8 @@ public class Connectfour extends PrintingGame {
                     win = true;
                 }
             }
-            // y=height
+
+            // y = height
             if (!win) {
                 n = 0;
                 for (x = 0; x <= fieldsx; x = x + 2) {
@@ -131,6 +172,7 @@ public class Connectfour extends PrintingGame {
                     }
                 }
             }
+
             // x
             if (!win && height <= fieldsy - (3 * 2)) {
                 x = width;
@@ -155,6 +197,7 @@ public class Connectfour extends PrintingGame {
                     y = y - 2;
                 }
             }
+
             // -x
             if (!win && height <= fieldsy - (3 * 2)) {
                 x = width;
@@ -179,9 +222,14 @@ public class Connectfour extends PrintingGame {
                     y = y - 2;
                 }
             }
-//end check win
-// control for the possibility of win
-            // y=0
+
+            /**
+             * control for a possibile win if 4 can be placed in a row in future
+             */
+            /**
+             * y = 0
+             * it is the only horizontal row to check
+             */
             if (!win) {
                 y = 0;
                 for (w = 0; w <= fieldsx - (3 * 2); w = w + 2) {
@@ -202,8 +250,11 @@ public class Connectfour extends PrintingGame {
                     }
                 }
             }
+
+            /**
+             * x = variabel check for every vertikal row only for the first 4
+             */
             if (!win && n != 4) {
-                // x=variabel
                 for (x = 0; x <= fieldsx; x = x + 2) {
                     first = 't';
                     n = 0;
@@ -222,8 +273,11 @@ public class Connectfour extends PrintingGame {
                     }
                 }
             }
+
+            /**
+             * -x check for every diagonl row
+             */
             if (!win && n != 4) {
-                // -x
                 for (w = 0; w <= fieldsx - (3 * 2); w = w + 2) {
                     y = 0;
                     first = 't';
@@ -244,8 +298,12 @@ public class Connectfour extends PrintingGame {
                     }
                 }
             }
+
+            /**
+             * x
+             * check for every diagonl row
+             */
             if (!win && n != 4) {
-                // x
                 for (w = fieldsx; w >= 0 + (3 * 2); w = w - 2) {
                     y = 0;
                     first = 't';
@@ -266,14 +324,19 @@ public class Connectfour extends PrintingGame {
                     }
                 }
             }
+
+            /**
+             * end game
+             */
             if (n != 4) {
-                p("Es gibt keine MÃ¶glichkeit mehr um zu gewinnen.");
-                abr = true;
+                p("Spätestens jetz besthet keine  MÃ¶glichkeit mehr um zu gewinnen.");
+                stop = true;
             }
-//end check possibility of win
-//switch players
+
+            /**
+             * switch players
+             */
             player = !player;
         }
-
     }
 }
