1a7e9d7ff9660489a68c9074cfbb9a335ce56326
Nils488 <60072911+Nils488@users.noreply.github.com>
2020-03-31 18:27:14 +0200

    überarbeitung
    dokumentiert unbd verbessert

--- src/Games/RPS.java
+++ src/Games/RPS.java
@@ -3,77 +3,87 @@ package Games;
 import printing.PrintingGame;
 
 public class RPS extends PrintingGame {
+
     public RPS() {
         super("Schere Stein Papier");
     }
 
     @Override
     public void run() {
-        // Benutzereingabe R/P/S
-        p("Für Schere: 1");
-        p("Für Stein: 2");
-        p("Für Papier: 3");
-        p("Wählen Sie eine der Folgenenden Optionen");
-        int opt = input.nextInt();
         
-        // zufällige Auswahl des Com Spielers
-        int zufall = randomInt(3) + 1;
-        // Auswertung des Ergebnisses
-        switch (zufall) {
+        /**
+         *deklaration and initaization
+         */
+        int opt = 0, com;
+
+        /**
+         * userinput R/P/S
+         */
+        do {
+            p("Für Schere: 1");
+            p("Für Stein: 2");
+            p("Für Papier: 3");
+            p("Wählen Sie eine der folgenenden Optionen.");
+            opt = input.nextInt();
+            if (opt < 1 || opt > 3){
+            p("Diese Optionen existiert nicht.");
+            }
+        } while (opt < 1 || opt > 3);
+
+        /**
+         * create a random number for the Com
+         */
+        com = randomInt(3) + 1;
+
+        /**
+         * compare the numbers
+         */
+        switch (com) {
             case 1:
-                p("Der Com wählt Schere");
+                p("Der Computer wählt Schere.");
                 switch (opt) {
                     case 1:
-                        p("Es ist unentschieden");
+                        p("Es ist unentschieden.");
                         break;
                     case 2:
-                        p("Sie haben gewonnen");
+                        p("Sie haben gewonnen.");
                         break;
                     case 3:
-                        p("Sie haben verloren");
-                        break;
-                    default:
-                        p("Sie haben eine falsche Option gewählt.");
+                        p("Sie haben verloren.");
                         break;
                 }
-            break;
+                break;
             case 2:
-                p("Der Com wählt Stein");
+                p("Der Computer wählt Stein.");
                 switch (opt) {
                     case 1:
-                        p("Sie haben verloren");
+                        p("Sie haben verloren.");
                         break;
                     case 2:
-                        p("Es ist unentschieden");
+                        p("Es ist unentschieden.");
                         break;
                     case 3:
-                        p("Sie haben gewonnen");
-                        break;
-                    default:
-                        p("Sie haben eine falsche Option gewählt.");
+                        p("Sie haben gewonnen.");
                         break;
                 }
-            break;
+                break;
             case 3:
-                p("Der Com wählt Papier");
+                p("Der Computer wählt Papier.");
                 switch (opt) {
                     case 1:
-                        p("Sie haben gewonnen");
+                        p("Sie haben gewonnen.");
                         break;
                     case 2:
-                        p("Sie haben verloren");
+                        p("Sie haben verloren.");
                         break;
                     case 3:
-                        p("Es ist unentschieden");
-                        break;
-                    default:
-                        p("Sie haben eine falsche Option gewählt.");
+                        p("Es ist unentschieden.");
                         break;
                 }
-            break;
+                break;
             default:
                 p("Die Entwickler haben Mist gebaut.");
-            break;
+                break;
         }
     }
 }
